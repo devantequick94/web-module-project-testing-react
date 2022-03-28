1e2bc48b3c57cdb15f4ba41f6440abd2b00079c4
devantequick <devantequick94@gmail.com>
2022-03-28 19:09:14 -0400

    epidsode tests passing

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -3,6 +3,23 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
 
+const testSghow = {
+    name: "test show",
+    summary "test summary",
+    season: [
+        {
+            id: 0,
+            name: "season 1"
+            episode: []
+        },
+        {
+            id: 1,
+            name: "season 2"
+            episode: []
+        }
+    ]
+};
+
 test('renders without errors', () => { });
 
 test('renders Loading component when prop show is null', () => { });
